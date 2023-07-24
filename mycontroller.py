202dc12b1ab59d39898488a37d4b04d9a6632d6f
Henrique Becker Brum <hbrum499@gmail.com>
2023-07-24 15:07:24 +0000

    Created write_rules function

--- mycontroller.py
+++ mycontroller.py
@@ -66,7 +66,7 @@ def main(args):
             switch.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
                                        bmv2_json_file_path=args.bmv2_json)
             switches[switch_id] = switch
-            write_rules(p4info_helper, switch_id, rules)   
+            write_rules(p4info_helper, switch, rules)   
             read_table_rules()         
 
         # # TODO Uncomment the following two lines to read table entries from s1 and s2
@@ -121,86 +121,72 @@ def distribute_rules(network_info, rules, strategy):
 
     return device_table_entries_map
 
-# Write the subset of table entries chosen for a swtich 
-def write_tunnel_rules(p4info_helper, ingress_sw, egress_sw, tunnel_id,
-                     dst_eth_addr, dst_ip_addr):
-    """
-    Installs three rules:
-    1) An tunnel ingress rule on the ingress switch in the ipv4_lpm table that
-       encapsulates traffic into a tunnel with the specified ID
-    2) A transit rule on the ingress switch that forwards traffic based on
-       the specified ID
-    3) An tunnel egress rule on the egress switch that decapsulates traffic
-       with the specified ID and sends it to the host
 
-    :param p4info_helper: the P4Info helper
-    :param ingress_sw: the ingress switch connection
-    :param egress_sw: the egress switch connection
-    :param tunnel_id: the specified tunnel ID
-    :param dst_eth_addr: the destination IP to match in the ingress rule
-    :param dst_ip_addr: the destination Ethernet address to write in the
-                        egress rule
-    """
-    # 1) Tunnel Ingress Rule
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.ipv4_ids",
-        match_fields={
-            "hdr.ipv4.dstAddr": (dst_ip_addr, 32)
-        },
-        action_name="MyIngress.myTunnel_ingress",
-        action_params={
-            "dst_id": tunnel_id,
-        })
-    ingress_sw.WriteTableEntry(table_entry)
-    print("Installed ingress tunnel rule on %s" % ingress_sw.name)
 
-    # 2) Tunnel Transit Rule
-    # The rule will need to be added to the myTunnel_exact table and match on
-    # the tunnel ID (hdr.myTunnel.dst_id). Traffic will need to be forwarded
-    # using the myTunnel_forward action on the port connected to the next switch.
-    #
-    # For our simple topology, switch 1 and switch 2 are connected using a
-    # link attached to port 2 on both switches. We have defined a variable at
-    # the top of the file, SWITCH_TO_SWITCH_PORT, that you can use as the output
-    # port for this action.
-    #
-    # We will only need a transit rule on the ingress switch because we are
-    # using a simple topology. In general, you'll need on transit rule for
-    # each switch in the path (except the last switch, which has the egress rule),
-    # and you will need to select the port dynamically for each switch based on
-    # your topology.
+def write_rules(p4info_helper, switch, rules):
+    for rule in rules:
+        rule_fields = get_rule_fields(rule)
 
-    # TODO build the transit rule
-    # TODO install the transit rule on the ingress switch
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.myTunnel_exact",
-        match_fields={
-            "hdr.myTunnel.dst_id": tunnel_id
-        },
-        action_name="MyIngress.myTunnel_forward",
-        action_params={
-            "port": SWITCH_TO_SWITCH_PORT
-        })
-    ingress_sw.WriteTableEntry(table_entry)
-    print("Installed transit tunnel rule on %s" % ingress_sw.name)
+        if rule_fields["table_name"] == "ipv4_ids":
+            table_entry = p4info_helper.buildTableEntry(
+                table_name="MyIngress.ipv4_ids",
+                match_fields={
+                    "hdr.ip.v4.protocol": rule_fields["protocol"],
+                    "hdr.ip.v4.srcAddr": (rule_fields["srcAddr"], rule_fields["srcMask"]),
+                    "meta.srcPort":(rule_fields["srcPortLower"], rule_fields["srcPortUpper"]),
+                    "hdr.ip.v4.dstAddr": (rule_fields["dstAddr"], rule_fields["dstMask"]),
+                    "meta.dstPort": (rule_fields["dstPortLower"], rule_fields["dstPortUpper"]),
+                    "meta.flags" (rule_fields["flags"], rule_fields["flagsMask"])
+                },
+                action_name="MyIngress."+rule_fields["action"],
+                action_params={
+                    "dst_id": tunnel_id,
+                })
+        else:
+            table_entry = p4info_helper.buildTableEntry(
+                table_name="MyIngress.ipv6_ids",
+                priority=rule_fields["priority"]
+                match_fields={
+                    "hdr.ip.v6.nextHeader": rule_fields["protocol"],
+                    "hdr.ip.v6.srcAddr": (rule_fields["srcAddr"], rule_fields["srcMask"]),
+                    "meta.srcPort":(rule_fields["srcPortLower"], rule_fields["srcPortUpper"]),
+                    "hdr.ip.v6.dstAddr": (rule_fields["dstAddr"], rule_fields["dstMask"]),
+                    "meta.dstPort": (rule_fields["dstPortLower"], rule_fields["dstPortUpper"]),
+                    "meta.flags" (rule_fields["flags"], rule_fields["flagsMask"])
+                },
+                action_name="MyIngress."+rule_fields["action"],
+                action_params={
+                    "port": rules_fields["new_port"],
+                })
 
-    # 3) Tunnel Egress Rule
-    # For our simple topology, the host will always be located on the
-    # SWITCH_TO_HOST_PORT (port 1).
-    # In general, you will need to keep track of which port the host is
-    # connected to.
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.myTunnel_exact",
-        match_fields={
-            "hdr.myTunnel.dst_id": tunnel_id
-        },
-        action_name="MyIngress.myTunnel_egress",
-        action_params={
-            "dstAddr": dst_eth_addr,
-            "port": SWITCH_TO_HOST_PORT
-        })
-    egress_sw.WriteTableEntry(table_entry)
-    print("Installed egress tunnel rule on %s" % egress_sw.name)
+        switch.WriteTableEntry(table_entry)
+
+def get_rule_fields(rule):
+    rule_fields = {}
+    rule_items = rule.split(" ")
+    
+    rule_fields["table_name"] = rule_items[1]
+    rule_fields["action"] = rule_items[2]
+    rule_fields["protocol"] = rule_items[3]
+
+    rule_fields["srcAddr"] = rule_items[4].split("&&&")[0]
+    rule_fields["srcMask"] = rule_items[4].split("&&&")[1]
+
+    rule_fields["srcPortLower"] = rule_items[5].split("->")[0]
+    rule_fields["srcPortUpper"] = rule_items[5].split("->")[1]
+
+    rule_fields["dstAddr"] = rule_items[6].split("&&&")[0]
+    rule_fields["dstMask"] = rule_items[6].split("&&&")[1]
+
+    rule_fields["dstPortLower"] = rule_items[7].split("->")[0]
+    rule_fields["dstPortUpper"] = rule_items[7].split("->")[1]
+
+    rule_fields["flags"] = rule_items[8].split("&&&")[0]
+    rule_fields["flagsMask"] = rule_items[8].split("&&&")[1]
+
+    rule_fields["new_port"] = rule_items[10]
+    rule_fields["priority"] = rule_items[11]
+
 
 
 
