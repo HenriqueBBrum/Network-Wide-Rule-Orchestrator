e53f6a7c485453c03f18daa4ca1abf16a628f1c3
Henrique B BRum <hbrum499@gmail.com>
2023-10-30 14:02:49 +0000

    Fixed dir structure and added snort rules and config

---
+++ snort/configuration/snort.lua
@@ -0,0 +1,321 @@
+---------------------------------------------------------------------------
+-- Snort++ configuration
+---------------------------------------------------------------------------
+
+-- there are over 200 modules available to tune your policy.
+-- many can be used with defaults w/o any explicit configuration.
+-- use this conf as a template for your specific configuration.
+
+-- 1. configure defaults
+-- 2. configure inspection
+-- 3. configure bindings
+-- 4. configure performance
+-- 5. configure detection
+-- 6. configure filters
+-- 7. configure outputs
+-- 8. configure tweaks
+
+---------------------------------------------------------------------------
+-- 1. configure defaults
+---------------------------------------------------------------------------
+
+HOME_NET = '192.168.10.0/24 172.16.0.0/16 205.174.165.68 205.174.165.66'
+
+EXTERNAL_NET = 'any'
+
+DNS_SERVERS = '192.168.10.3/32'
+
+SMTP_SERVERS = HOME_NET
+
+HTTP_SERVERS = HOME_NET
+
+SQL_SERVERS = HOME_NET
+
+TELNET_SERVERS = HOME_NET
+
+SSH_SERVERS = HOME_NET
+
+FTP_SERVERS = HOME_NET
+
+SIP_SERVERS = HOME_NET
+
+HTTP_PORTS = [[ 36 80 81 82 83 84 85 86 87 88 89 90 311 383 443 555 591 593
+    631 801 808 818 901 972 1158 1220 1414 1533 1581 1719 1720 1741 1801 1830
+    1942 2231 2301 2375 2381 2578 2809 2869 2980 3000 3029 3037 3057 3128
+    3443 3702 4000 4343 4592 4848 5000 5054 5060 5061 5117 5222 5250 5416
+    5443 5450 5555 5600 5814 5894 5984 6080 6173 6988 7000 7001 7005 7070
+    7071 7144 7145 7180 7181 7510 7770 7777 7778 7779 8000 8001 8008 8014
+    8015 8020 8028 8040 8080 8081 8082 8085 8088 8090 8095 8118 8123 8180
+    8181 8182 8222 8243 8280 8300 8333 8344 8393 8400 8443 8484 8500 8509
+    8694 8787 8800 8852 8880 8888 8899 8983 9000 9002 9060 9080 9090 9091
+    9111 9200 9201 9290 9443 9447 9700 9710 9788 9830 9850 9999 10000 10080
+    10100 10250 10255 10297 10443 11371 12601 13014 14592 15489 16000 17000
+    18081 19980 29991 30007 30018 33300 34412 34443 34444 36099 40007 41080
+    44449 49152 49153 50000 50002 50452 51423 53331 55252 55555 56712 ]]
+
+SHELLCODE_PORTS = '!80'
+
+MAIL_PORTS = '110 143'
+
+ORACLE_PORTS = '1024:'
+
+SSH_PORTS = '22'
+
+FTP_PORTS = '21 2100 3535'
+
+SIP_PORTS = '5060 5061 5600'
+
+FILE_DATA_PORTS = HTTP_PORTS .. ' 110 143'
+
+GTP_PORTS = '2123 2152 3386'
+
+AIM_SERVERS = [[ 64.12.24.0/23 64.12.28.0/23 64.12.161.0/24 64.12.163.0/24
+    64.12.200.0/24 205.188.3.0/24 205.188.5.0/24 205.188.7.0/24
+    205.188.9.0/24 205.188.153.0/24 205.188.179.0/24 205.188.248.0/24 ]]
+
+include 'snort_defaults.lua'
+
+---------------------------------------------------------------------------
+-- 2. configure inspection
+---------------------------------------------------------------------------
+
+-- mod = { } uses internal defaults
+-- you can see them with snort --help-module mod
+
+-- mod = default_mod uses external defaults
+-- you can see them in snort_defaults.lua
+
+-- the following are quite capable with defaults:
+
+stream = { }
+stream_ip = { }
+stream_icmp = { }
+stream_tcp = { }
+stream_udp = { }
+stream_user = { }
+stream_file = { }
+
+arp_spoof = { }
+back_orifice = { }
+dns = { }
+imap = { }
+netflow = {}
+normalizer = { }
+pop = { }
+rpc_decode = { }
+sip = { }
+ssh = { }
+ssl = { }
+telnet = { }
+
+cip = { }
+dnp3 = { }
+iec104 = { }
+mms = { }
+modbus = { }
+s7commplus = { }
+
+dce_smb = { }
+dce_tcp = { }
+dce_udp = { }
+dce_http_proxy = { }
+dce_http_server = { }
+
+-- see snort_defaults.lua for default_*
+gtp_inspect = default_gtp
+port_scan = default_med_port_scan
+smtp = default_smtp
+
+ftp_server = default_ftp_server
+ftp_client = { }
+ftp_data = { }
+
+http_inspect = default_http_inspect
+http2_inspect = { }
+
+-- see file_magic.rules for file id rules
+file_id = { rules_file = 'file_magic.rules' }
+file_policy = { }
+
+-- the following require additional configuration to be fully effective:
+
+appid =
+{
+    -- appid requires this to use appids in rules
+    --app_detector_dir = 'directory to load appid detectors from'
+}
+
+--[[
+reputation =
+{
+    -- configure one or both of these, then uncomment reputation
+    -- (see also related path vars at the top of snort_defaults.lua)
+
+    --blacklist = 'blacklist file name with ip lists'
+    --whitelist = 'whitelist file name with ip lists'
+}
+--]]
+
+---------------------------------------------------------------------------
+-- 3. configure bindings
+---------------------------------------------------------------------------
+
+wizard = default_wizard
+
+binder =
+{
+    -- port bindings required for protocols without wizard support
+    { when = { proto = 'udp', ports = '53', role='server' },  use = { type = 'dns' } },
+    { when = { proto = 'tcp', ports = '53', role='server' },  use = { type = 'dns' } },
+    { when = { proto = 'tcp', ports = '102', role = 'server' }, use = { type = 's7commplus' } },
+    { when = { proto = 'tcp', ports = '111', role='server' }, use = { type = 'rpc_decode' } },
+    { when = { proto = 'tcp', ports = '502', role='server' }, use = { type = 'modbus' } },
+    { when = { proto = 'tcp', ports = '2123 2152 3386', role='server' }, use = { type = 'gtp_inspect' } },
+    { when = { proto = 'tcp', ports = '2404', role='server' }, use = { type = 'iec104' } },
+    { when = { proto = 'udp', ports = '22222', role = 'server' }, use = { type = 'cip' } },
+    { when = { proto = 'tcp', ports = '44818', role = 'server' }, use = { type = 'cip' } },
+
+    { when = { proto = 'tcp', service = 'dcerpc' },  use = { type = 'dce_tcp' } },
+    { when = { proto = 'udp', service = 'dcerpc' },  use = { type = 'dce_udp' } },
+    { when = { proto = 'udp', service = 'netflow' }, use = { type = 'netflow' } },
+
+    { when = { service = 'netbios-ssn' },      use = { type = 'dce_smb' } },
+    { when = { service = 'dce_http_server' },  use = { type = 'dce_http_server' } },
+    { when = { service = 'dce_http_proxy' },   use = { type = 'dce_http_proxy' } },
+
+    { when = { service = 'cip' },              use = { type = 'cip' } },
+    { when = { service = 'dnp3' },             use = { type = 'dnp3' } },
+    { when = { service = 'dns' },              use = { type = 'dns' } },
+    { when = { service = 'ftp' },              use = { type = 'ftp_server' } },
+    { when = { service = 'ftp-data' },         use = { type = 'ftp_data' } },
+    { when = { service = 'gtp' },              use = { type = 'gtp_inspect' } },
+    { when = { service = 'imap' },             use = { type = 'imap' } },
+    { when = { service = 'http' },             use = { type = 'http_inspect' } },
+    { when = { service = 'http2' },            use = { type = 'http2_inspect' } },
+    { when = { service = 'iec104' },           use = { type = 'iec104' } },
+    { when = { service = 'mms' },              use = { type = 'mms' } },
+    { when = { service = 'modbus' },           use = { type = 'modbus' } },
+    { when = { service = 'pop3' },             use = { type = 'pop' } },
+    { when = { service = 'ssh' },              use = { type = 'ssh' } },
+    { when = { service = 'sip' },              use = { type = 'sip' } },
+    { when = { service = 'smtp' },             use = { type = 'smtp' } },
+    { when = { service = 'ssl' },              use = { type = 'ssl' } },
+    { when = { service = 'sunrpc' },           use = { type = 'rpc_decode' } },
+    { when = { service = 's7commplus' },       use = { type = 's7commplus' } },
+    { when = { service = 'telnet' },           use = { type = 'telnet' } },
+
+    { use = { type = 'wizard' } }
+}
+
+---------------------------------------------------------------------------
+-- 4. configure performance
+---------------------------------------------------------------------------
+
+-- use latency to monitor / enforce packet and rule thresholds
+--latency = { }
+
+-- use these to capture perf data for analysis and tuning
+--profiler = { }
+--perf_monitor = { }
+
+---------------------------------------------------------------------------
+-- 5. configure detection
+---------------------------------------------------------------------------
+
+references = default_references
+classifications = default_classifications
+
+ips =
+{
+    -- use this to enable decoder and inspector alerts
+    --enable_builtin_rules = true,
+
+    -- use include for rules files; be sure to set your path
+    -- note that rules files can include other rules files
+    -- (see also related path vars at the top of snort_defaults.lua)
+
+    variables = default_variables
+}
+
+-- use these to configure additional rule actions
+-- react = { }
+-- reject = { }
+
+-- use this to enable payload injection utility
+-- payload_injector = { }
+
+---------------------------------------------------------------------------
+-- 6. configure filters
+---------------------------------------------------------------------------
+
+-- below are examples of filters
+-- each table is a list of records
+
+--[[
+suppress =
+{
+    -- don't want to any of see these
+    { gid = 1, sid = 1 },
+
+    -- don't want to see anything for a given host
+    { track = 'by_dst', ip = '1.2.3.4' }
+
+    -- don't want to see these for a given host
+    { gid = 1, sid = 2, track = 'by_dst', ip = '1.2.3.4' },
+}
+--]]
+
+--[[
+event_filter =
+{
+    -- reduce the number of events logged for some rules
+    { gid = 1, sid = 1, type = 'limit', track = 'by_src', count = 2, seconds = 10 },
+    { gid = 1, sid = 2, type = 'both',  track = 'by_dst', count = 5, seconds = 60 },
+}
+--]]
+
+--[[
+rate_filter =
+{
+    -- alert on connection attempts from clients in SOME_NET
+    { gid = 135, sid = 1, track = 'by_src', count = 5, seconds = 1,
+      new_action = 'alert', timeout = 4, apply_to = '[$SOME_NET]' },
+
+    -- alert on connections to servers over threshold
+    { gid = 135, sid = 2, track = 'by_dst', count = 29, seconds = 3,
+      new_action = 'alert', timeout = 1 },
+}
+--]]
+
+---------------------------------------------------------------------------
+-- 7. configure outputs
+---------------------------------------------------------------------------
+
+-- event logging
+-- you can enable with defaults from the command line with -A <alert_type>
+-- uncomment below to set non-default configs
+--alert_csv = { }
+--alert_fast = { }
+--alert_full = { }
+--alert_sfsocket = { }
+--alert_syslog = { }
+--unified2 = { }
+
+-- packet logging
+-- you can enable with defaults from the command line with -L <log_type>
+--log_codecs = { }
+--log_hext = { }
+--log_pcap = { }
+
+-- additional logs
+--packet_capture = { }
+--file_log = { }
+
+---------------------------------------------------------------------------
+-- 8. configure tweaks
+---------------------------------------------------------------------------
+
+if ( tweaks ~= nil ) then
+    include(tweaks .. '.lua')
+end
+
---
+++ src/main.p4
@@ -0,0 +1,261 @@
+#include <core.p4>
+#include <v1model.p4>
+
+#include "include/header.p4"
+#include "include/parser.p4"
+
+
+control MyIngress(inout headers hdr, inout metadata meta, inout standard_metadata_t standard_metadata){
+    // Statistics counters
+    counter(64, CounterType.packets) received;
+    counter(64, CounterType.packets) ids_flow;
+
+    direct_counter(CounterType.packets_and_bytes) ipv4_ids_table_hit_counter;
+    direct_counter(CounterType.packets_and_bytes) ipv6_ids_table_hit_counter;
+
+    // Registers for the count-min sketch
+    register<bit<8>>(4) cm_limiter1;
+    register<bit<8>>(4) cm_limiter2;
+    register<bit<8>>(4) cm_limiter3;
+    register<bit<8>>(4) cm_limiter4;
+
+
+    register<bit<16>>(4) window_id_tracker1;
+    register<bit<16>>(4) window_id_tracker2;
+    register<bit<16>>(4) window_id_tracker3;
+    register<bit<16>>(4) window_id_tracker4;
+
+    register<bit<16>>(1) global_window_tracker;
+
+    // Variables used in IDS fowarding logic
+    bit<8> current_min = 0;
+
+    // Variables for flow expiring mechanism
+    bit<48> last_timestamp = 0;
+    register<bit<48>>(1) last_packet_timestamp;
+
+
+    /**** Actions ****/
+
+    action pass() {
+        meta.ids_table_match = false;
+    }
+
+    action clone_to_ids() {
+        meta.ids_table_match = true;
+    }
+
+    /**** Tables ****/
+
+    table ipv4_ids {
+        actions = {
+            pass;
+            clone_to_ids;
+            NoAction;
+        }
+        key = {
+           meta.protocol: exact;
+           hdr.ip.v4.srcAddr: ternary;
+           meta.srcPort: range;
+           hdr.ip.v4.dstAddr: ternary;
+           meta.dstPort: range;
+           meta.flags: exact;
+        }
+        size = 10240;
+        default_action = pass();
+        counters = ipv4_ids_table_hit_counter;
+    }
+
+    table ipv6_ids{
+        actions = {
+            pass;
+            clone_to_ids;
+            NoAction;
+        }
+        key = {
+           meta.protocol: exact;
+           hdr.ip.v6.srcAddr: ternary;
+           meta.srcPort: range;
+           hdr.ip.v6.dstAddr: ternary;
+           meta.dstPort: range;
+           meta.flags: exact;
+        }
+        size = 10240;
+        default_action = pass();
+        counters = ipv6_ids_table_hit_counter;
+    }
+
+    /**** Independent actions ****/
+
+
+    action get_ports(){
+        if(hdr.ip_encapsulated_proto.icmp.isValid()){
+            meta.srcPort = 0;
+            meta.dstPort = 0;
+            meta.flags = 0;
+        }else if(hdr.ip_encapsulated_proto.tcp.isValid()){
+            meta.srcPort = hdr.ip_encapsulated_proto.tcp.srcPort;
+            meta.dstPort = hdr.ip_encapsulated_proto.tcp.dstPort;
+            meta.flags = hdr.ip_encapsulated_proto.tcp.flags;
+        }else if(hdr.ip_encapsulated_proto.udp.isValid()){
+            meta.srcPort = hdr.ip_encapsulated_proto.udp.srcPort;
+            meta.dstPort = hdr.ip_encapsulated_proto.udp.dstPort;
+            meta.flags = 0;
+        }
+    }
+
+
+     // Reads the count-min sketch and returns the min count found in all four rows
+    action read_cm_limiter(bit<16> protocol, bit<128> src_ip, bit<128> dst_ip, bit<16> src_port, bit<16> dst_port) {
+        bit<32> flow_hash1;
+        bit<32> flow_hash2;
+        bit<32> flow_hash3;
+        bit<32> flow_hash4;
+
+        hash(flow_hash1, HashAlgorithm.crc16, 32w0, {src_ip, dst_ip, src_port, dst_port, protocol}, 32w4);
+        hash(flow_hash2, HashAlgorithm.csum16, 32w0, {src_ip, dst_ip, src_port, dst_port, protocol}, 32w4);
+        hash(flow_hash3, HashAlgorithm.crc16_custom, 32w0, {src_ip, dst_ip, src_port, dst_port, protocol}, 32w4);
+        hash(flow_hash4, HashAlgorithm.crc32, 32w0, {src_ip, dst_ip, src_port, dst_port, protocol}, 32w4);
+
+        current_min = 0xFF;
+        bit<8> aux;
+        cm_limiter1.read(aux, flow_hash1);
+        current_min = aux < current_min ? aux : current_min;
+
+        cm_limiter2.read(aux, flow_hash2);
+        current_min = aux < current_min ? aux : current_min;
+
+        cm_limiter3.read(aux, flow_hash3);
+        current_min = aux < current_min ? aux : current_min;
+
+        cm_limiter4.read(aux, flow_hash4);
+        current_min = aux < current_min ? aux : current_min;
+
+        log_msg("cm_limiter minimum value {}", {current_min});
+    }
+
+
+    // Updates the entries for a flow in the count-min sketch if a new packet from the flow has arrived
+    action increment_cm_limiter(bit<16> protocol, bit<128> src_ip, bit<128> dst_ip, bit<16> src_port, bit<16> dst_port) {
+        bit<32> flow_hash1;
+        bit<32> flow_hash2;
+        bit<32> flow_hash3;
+        bit<32> flow_hash4;
+
+        hash(flow_hash1, HashAlgorithm.crc16, 32w0, {src_ip, dst_ip, src_port, dst_port, protocol}, 32w4);
+        hash(flow_hash2, HashAlgorithm.csum16, 32w0, {src_ip, dst_ip, src_port, dst_port, protocol}, 32w4);
+        hash(flow_hash3, HashAlgorithm.crc16_custom, 32w0, {src_ip, dst_ip, src_port, dst_port, protocol}, 32w4);
+        hash(flow_hash4, HashAlgorithm.crc32, 32w0, {src_ip, dst_ip, src_port, dst_port, protocol}, 32w4);
+
+
+        bit<16> global_window_id = 0;
+        global_window_tracker.read(global_window_id, 0);
+
+        bit<8> aux_counter;
+        bit<16> aux_window_id_tracker;
+
+        // Update count min row 1
+        cm_limiter1.read(aux_counter, flow_hash1);
+        log_msg("cm_limiter1 new value {}", {aux_counter + 1});
+        window_id_tracker1.read(aux_window_id_tracker, flow_hash1);
+        cm_limiter1.write(flow_hash1,  (global_window_id - aux_window_id_tracker) >= 2? 1 :(aux_counter < MAX_PACKETS ? aux_counter + 1 : MAX_PACKETS+1));
+
+        // Update count min row 2
+        cm_limiter2.read(aux_counter, flow_hash2);
+        log_msg("cm_limiter2 new value {}", {aux_counter + 1});
+        window_id_tracker2.read(aux_window_id_tracker, flow_hash2);
+        cm_limiter2.write(flow_hash2,  (global_window_id - aux_window_id_tracker) >= 2? 1 :(aux_counter < MAX_PACKETS ? aux_counter + 1 : MAX_PACKETS+1));
+
+        // Update count min row 3
+        cm_limiter3.read(aux_counter, flow_hash3);
+        log_msg("cm_limiter3 new value {}", {aux_counter + 1});
+        window_id_tracker3.read(aux_window_id_tracker, flow_hash3);
+        cm_limiter3.write(flow_hash3,  (global_window_id - aux_window_id_tracker) >= 2? 1 :(aux_counter < MAX_PACKETS ? aux_counter + 1 : MAX_PACKETS+1));
+
+        // Update count min row 4
+        cm_limiter4.read(aux_counter, flow_hash4);
+        log_msg("cm_limiter4 new value {}", {aux_counter + 1});
+        window_id_tracker4.read(aux_window_id_tracker, flow_hash4);
+        cm_limiter4.write(flow_hash4,  (global_window_id - aux_window_id_tracker) >= 2? 1 :(aux_counter < MAX_PACKETS ? aux_counter + 1 : MAX_PACKETS+1));
+
+
+        window_id_tracker1.write(flow_hash1, global_window_id);
+        window_id_tracker2.write(flow_hash2, global_window_id);
+        window_id_tracker3.write(flow_hash3, global_window_id);
+        window_id_tracker4.write(flow_hash4, global_window_id);
+    }
+
+
+    // **** Aply block ****
+    apply {
+        received.count((bit<32>)standard_metadata.ingress_port);
+        standard_metadata.egress_spec = DEFAULT_PORT;
+
+        if (hdr.ip.v4.isValid() || hdr.ip.v6.isValid()){
+            bit<128> src_IP = 0;
+            bit<128> dst_IP = 0;
+
+            if (hdr.ip.v4.isValid()){
+                src_IP = (bit<128>)hdr.ip.v4.srcAddr;
+                dst_IP = (bit<128>)hdr.ip.v4.dstAddr;
+            }else if(hdr.ip.v6.isValid()){
+                src_IP = hdr.ip.v6.srcAddr;
+                dst_IP = hdr.ip.v6.dstAddr;
+            }
+
+
+            // Checks if packet match an IDS rule and determines the approriate egress port
+            if (hdr.ip.v4.isValid()){
+               ipv4_ids.apply();
+            }else if(hdr.ip.v6.isValid()){
+               ipv6_ids.apply();
+            }
+
+            if(meta.ids_table_match){
+                increment_cm_limiter(meta.protocol, src_IP, dst_IP, meta.srcPort, meta.dstPort);
+                read_cm_limiter(meta.protocol, src_IP, dst_IP, meta.srcPort, meta.dstPort);
+                // If this flow ID is not in Count-min Sketch, meaning it is an unknown flow
+                if(current_min == 1){
+                    ids_flow.count((bit<32>) 1);
+                    clone(CloneType.I2E, REPORT_MIRROR_SESSION_ID);
+                // This flow is already present at the Count-min Sketch but it's still needed to foward some of the flow's packets to the IDS
+                }else if(current_min <= MAX_PACKETS){
+                    clone(CloneType.I2E, REPORT_MIRROR_SESSION_ID);
+                }
+                // This flow is already present at the Count-min Sketch and it's not needed to foward packets from this flow to the IDS
+            }
+
+            last_packet_timestamp.read(last_timestamp, 0);
+            bit<48> time_diff = standard_metadata.ingress_global_timestamp - last_timestamp;
+            if (time_diff > ONE_SECOND * 10) {
+                bit<16> global_window_id = 0;
+                global_window_tracker.read(global_window_id, 0);
+                global_window_id = global_window_id + 1;
+                global_window_tracker.write(0, global_window_id); 
+
+                log_msg("New timeout");
+                last_packet_timestamp.write(0, standard_metadata.ingress_global_timestamp); // update
+            }
+        }
+    }
+}
+
+
+control MyEgress(inout headers hdr, inout metadata meta, inout standard_metadata_t standard_metadata){
+    counter(64, CounterType.packets) cloned_to_ids;
+
+    apply {
+        if (hdr.ip.v4.isValid() || hdr.ip.v6.isValid()){
+            if(standard_metadata.instance_type == PKT_INSTANCE_TYPE_INGRESS_CLONE){
+                standard_metadata.egress_spec = PORT_TO_IDS;
+            }
+
+            cloned_to_ids.count((bit<32>) standard_metadata.egress_spec);
+        }
+
+
+    }
+}
+
+
+V1Switch(MyParser(), MyVerifyChecksum(), MyIngress(), MyEgress(), MyComputeChecksum(), MyDeparser()) main;
