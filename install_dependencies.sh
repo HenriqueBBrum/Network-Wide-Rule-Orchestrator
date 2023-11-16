ea24890d0072a926fbb76240ad7f21e8bb8c961d
HenriqueBBrum <p4@p4>
2023-11-16 20:44:34 +0000

    Improved installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-printf "-------------------------- Creating output folder --------------------------"
-mkdir ../experiments_output
+printf "\n####### CREATING OUTPUT FOLDER #######\n"
+mkdir experiments_output
 
 
 # Install TCPreplay
-printf "\n-------------------------- Installing tcpreplay --------------------------"
+printf "\n####### INSTALLING TCPREPLAY #######\n"
 sudo apt install -y tcpreplay
 
 
 # Install python libs
-printf "\n-------------------------- Installing python packages --------------------------"
+printf "\n####### INSTALLING PYTHON PACKAGES #######\n"
 sudo pip install networkx
 sudo pip install matplotlib
 
 
 # Download BMV2 to build the performance version
-printf "\n-------------------------- Cloning bmv2 --------------------------"
-cd ../..
+printf "\n####### CLONING BMV2 #######\n"
+cd ../
 git clone https://github.com/p4lang/behavioral-model.git
 
-printf "\n-------------------------- Configuring and installing bmv2 for peformance execution --------------------------"
+printf "\n####### CONFIGURING AND INSTALLING BMV2 FOR PEFORMANCE #######\n"
 cd behavioral-model/
 /bin/bash install_deps.sh
 sudo apt-get install -y libreadline-dev
@@ -30,7 +30,7 @@ make
 sudo make install
 sudo ldconfig
 
-printf "\n-------------------------- Configuring and installing simple_switch_grpc --------------------------"
+printf "\n####### CONFIGURING AND INSTALLING SIMPLE_SWITCH_GRPC #######\n"
 cd targets/simple_switch_grpc
 /bin/bash configure --with-thrift 'CXXFLAGS=-O0 -g'
 make
@@ -39,16 +39,16 @@ sudo ldconfig
 
 
 # Install snort
-printf "\n-------------------------- Creating snort files folder --------------------------"
+printf "\n####### CREATING SNORT FILES FOLDER #######\n"
 mkdir ~/snort
 cd ~/snort
 
-printf "\n-------------------------- Installing libs and packages snort3 needs --------------------------"
+printf "\n####### INSTALLING LIBS AND PACKAGES SNORT3 NEEDS #######\n"
 sudo apt install build-essential libpcap-dev libpcre3-dev libnet1-dev zlib1g-dev luajit hwloc libdnet-dev libdumbnet-dev bison flex liblzma-dev openssl \
  libssl-dev pkg-config libhwloc-dev cmake cpputest libsqlite3-dev uuid-dev libcmocka-dev libnetfilter-queue-dev libmnl-dev autotools-dev libluajit-5.1-dev \
  libunwind-dev libfl-dev -y
 
-printf "\n-------------------------- Configuring and installing libdaq --------------------------"
+printf "\n####### CONFIGURING AND INSTALLING LIBDAQ #######\n"
 git clone https://github.com/snort3/libdaq.git
 cd libdaq
 ./bootstrap
@@ -56,7 +56,7 @@ cd libdaq
 make
 sudo make install
 
-printf "\n-------------------------- Installing gperftools --------------------------"
+printf "\n####### INSTALLING GPERFTOOLS #######\n"
 cd ../
 wget https://github.com/gperftools/gperftools/releases/download/gperftools-2.9.1/gperftools-2.9.1.tar.gz
 tar xzf gperftools-2.9.1.tar.gz
@@ -65,7 +65,7 @@ cd gperftools-2.9.1/
 make
 sudo make install
 
-printf "\n-------------------------- Installing snort3 --------------------------"
+printf "\n####### INSTALLING SNORT3 #######\n"
 cd ../
 wget https://github.com/snort3/snort3/archive/refs/heads/master.zip
 unzip master.zip
