b0351e1163cb9b58638a9636555b3aa03789d265
HenriqueBBrum <p4@p4>
2023-11-22 16:12:16 +0000

    Fixed rulesets

--- testing/parse_output.py
+++ testing/parse_output.py
@@ -8,15 +8,14 @@ def parse_args():
     parser = argparse.ArgumentParser(description='P4Runtime Controller')
     parser.add_argument('--input-folder', help='Folder containing the experiments json output files', type=str, required=True)
     parser.add_argument('--baseline-folder', help='Folder containing the baseline results', 
-    	type=str, default="../baseline_alerts/CICIDS2017/alerts_snort3-community", required=False)
+    	type=str, default="../baseline_alerts/CICIDS2017/alerts_snort2-emerging", required=False)
 
     return parser.parse_args()
 
 
 def main(args):
-	print(args)
 	experiments_data = read_experiments_data(args.input_folder)
-	print(experiments_data.keys())
+	# print(experiments_data.keys())
 	compare_with_baseline(experiments_data, args.baseline_folder)
 
 
@@ -33,7 +32,7 @@ def read_experiments_data(experiments_data_folder):
 		print(item)
 		for subdir in os.listdir(item_fullpath):
 			alert_file = os.path.join(item_fullpath, subdir) + "/alert_json.txt"
-			data, counter =  read_snort_alerts(alert_file)
+			data, counter, no_duplicates_data =  read_snort_alerts(alert_file)
 			raw_data.extend(data)
 			rules_counter.update(counter)
 
@@ -53,41 +52,39 @@ def read_snort_alerts(alert_file_path):
 				parsed_line = json.loads(line)
 				data.append(parsed_line)
 
-				entry_key = str(parsed_line["pkt_num"]) + parsed_line["rule"]
+				entry_key = str(parsed_line["pkt_num"]) + parsed_line["rule"] + parsed_line["timestamp"]
 				if entry_key not in no_duplicates_data:
 					no_duplicates_data[entry_key] = line
-				else:
-					print(no_duplicates_data[entry_key])
-					print(line)
-					print("Error!!!!!!!!!!!! Duplicated packet")
-					exit(-1)
+				# else:
+				# 	print(no_duplicates_data[entry_key])
+				# 	print(line)
+				# 	print("Error!!!!!!!!!!!! Duplicated packet")
+				# 	#exit(-1)
 				 
 				counter[parsed_line["rule"]]+=1
 		except Exception as e:
 			print("JSON error: ", e)
 	file.close()
-	return data, counter
+	return data, counter, no_duplicates_data
 
 def compare_with_baseline(experiments_data, baseline_folder):
 	baseline_data = {}
 	for alert_file in os.listdir(baseline_folder):
-		raw_data = []
-		rules_counter = Counter()
 		item_fullpath = os.path.join(baseline_folder, alert_file)
 
-		print(alert_file)
-		data, counter, no_duplicates_data =  read_snort_alerts(item_fullpath)
+		print(alert_file.split(".")[0])
+		raw_data, rules_counter, no_duplicates_data =  read_snort_alerts(item_fullpath)
 	
-		print(len(raw_data), len(unique_data))
+		print(len(raw_data), len(no_duplicates_data))
 		print(rules_counter.most_common(5))
-		baseline_data[item]=raw_data
+		baseline_data[alert_file.split(".")[0]]=raw_data
 
-	for pcap_name, data in baseline_data.items():
-		index = 0
-		while index < len(data):
-			print(data[index])
-			print(experiments_data[pcap_name][index])
-			break
+	# for pcap_name, data in baseline_data.items():
+	# 	index = 0
+	# 	while index < len(data):
+	# 		print(data[index])
+	# 		print(experiments_data[pcap_name][index])
+	# 		break
 
 
 
