fa92e43444dc323a397255f24b64b9bd928a302b
HenriqueBBrum <p4@p4>
2023-12-08 13:36:53 +0000

    Small fix

--- testing/main.sh
+++ testing/main.sh
@@ -31,6 +31,7 @@ for size in {4096,16384}; do
 	for packets_redirected in {25,50,100,200,400,800}; do
 		results_folder=${output_folder}${packets_redirected}_${time_threshold}_${size}_registered/
 		mkdir $results_folder
-		./run_experiment.sh $topology $results_folder $packets_redirected $time_threshold $size $ruleset_folder > full_output.txt
+		./run_experiment.sh $topology $results_folder $packets_redirected $time_threshold $size $ruleset_folder > $results_folder"full_output.txt"
+
 	done;
 done;
